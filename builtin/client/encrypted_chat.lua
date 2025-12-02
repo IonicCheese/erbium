3088d0428ad2f6049ebe716a48c73948020451cf
a-bad-dev <totallycalledjohn@gmail.com>
2025-12-02 03:39:59 -0400

    Convert from CSM to builtin

--- builtin/client/encrypted_chat.lua
+++ builtin/client/encrypted_chat.lua
@@ -1,6 +1,6 @@
 -- secret messages
--- (using formerly b64 but now using A E S)
-local MOD_PATH = core.get_modpath("ttd_aes")
+-- (using formerly base64 but now using A E S)
+local MOD_PATH = core.get_builtin_path() .. "client"
 
 local aes = dofile(MOD_PATH.."aes.lua").packaged
 
@@ -14,47 +14,6 @@ local function hex_to_string(hex_str)
     end))
 end
 
-local key = hex_to_string("2a2305e78c34997168bed08c6434ffd")
+local key = hex_to_string("2a2305e78c34997168bed08c6434ffd") -- this will do for now...
 
-local sanity_sucess = false
-
--- you should hopefully never see these errors
-local function sanity_check()
-    local message = "TTD.AES.SANITY_CHECK: Message for AES sanity check, !(%)#@)%^}|{/,.}°*~`|><,+™."
-    local encoded, err = aes.encrypt(message, key)
-    local encoded2, err = aes.encrypt(message, key)
-
-    if not encoded or not encoded2 then
-        return false, core.colorize("red", "[-!-] CSM.TTD.AES: SANITY.CHECK: ENCRYPT.ERROR: ") .."Failed to encrypt, "..err
-    end
-
-    local decoded, err = aes.decrypt(encoded, key)
-
-    if not decoded then
-        return false, core.colorize("red", "[-!-] CSM.TTD.AES: SANITY.CHECK: DECRYPT.ERROR: ") .."Failed to decrypt, "..err
-    end
-
-    if encoded == encoded2 then
-        sanity_sucess = true
-        return false, core.colorize("orange", "[-!-] CSM.TTD.AES: SANITY.CHECK: SECURITY.WARNING: ") .."Failed to generate second IV, TTD.AES will still run but wont be as secure"
-    end
-    
-    if decoded == message then
-        sanity_sucess = true
-        return true, core.colorize("lime", "[-!-] CSM.TTD.AES: SANITY.CHECK: ") .. "Sanity Check complete, TTD.AES is functioning correctly"
-    end
-end
-
-core.register_chatcommand("b", {})
-core.register_chatcommand("fb", {})
-
-core.after(0.5, function()
-    local success, result = sanity_check()
-    core.display_chat_message(result)
-
-    if not success then
-        core.log("error", result)
-    end
-
-    dofile(MOD_PATH .. "definition.lua")
-end)
+core.register_chatcommand("aes", {}) -- overridden in definition.lua
